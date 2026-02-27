02f8e417b35718508b8fa1b1a0137ed5cebc51e2
LoicStevens <loickamga580@gmail.com>
2026-02-27 22:53:07 +0100

    forgot-passwordUpdate

--- Finova/src/pages/forgot-password.tsx
+++ Finova/src/pages/forgot-password.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { Link } from "react-router-dom";
 import { FiMail } from "react-icons/fi";
 import ForgotImg from "../assets/forgot.jpg";
